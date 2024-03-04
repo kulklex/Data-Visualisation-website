757ada25bde61ab8ab0220fe504b6bf6a9fe6a33
Hassan Yusuff <kulklex@gmail.com>
2024-03-04 14:43:10 +0000

    Added processSentiments Function

--- lambdaFunctions/processSentiments.js
+++ lambdaFunctions/processSentiments.js
@@ -8,75 +8,8 @@ import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 const client = new DynamoDBClient({ region: "us-east-1" }); // AWS region
 const docClient = DynamoDBDocumentClient.from(client);
 
-// API URL
-const apiUrl = 'https://newsapi.org/v2/everything';
-
-// Function to create dynamic API parameters
-const createApiParams = (query) => ({
-  q: query,
-  sortBy: 'popularity',
-  language: 'en',
-  apiKey: 'bc1697d5f460435fbf9be668688ee620', // API key 
-});
-
-
 export const handler = async (event) => {
-  
-  const makeApiCall = async (query) => {
-  try {
-    // Create dynamic parameters based on the query
-    const params = createApiParams(query);
-
-    // Make a GET request using axios
-    const response = await axios.get(apiUrl, { params });
-    
-     // Handle the API response
-    const articles = response.data?.articles;
-    if (articles?.length) {
-      articles.map(async (article) => {
-        const dateString = article.publishedAt
-        const dateInMilliSeconds = convertDateToMilliseconds(dateString)
-
-      // Saving articles to FootballNews Table 
-        const command = new PutCommand({
-          TableName: "FootballNews",
-          Item: {
-            "MatchTS": dateInMilliSeconds,
-            "News": article.title,
-            "TeamName": query,
-            "Url": article.url
-          }
-        })
-        try {
-          // Store data in DynamoDB
-          await docClient.send(command);
-          console.log(query + " updated");
-
-        } catch (err) {
-          console.error("Error saving data: ", err);
-        }
-
-      })
-    }
-  } catch (error) {
-    // Handle errors
-    console.error('News API Call Error: ', error);
-  }
-};
-
-// Function that converts date string to milliseconds
-function convertDateToMilliseconds(dateString) {
-  const dateInMilliSeconds = new Date(dateString).getTime();
-  return dateInMilliSeconds;
-}
-
-
-// API calls for different football teams
-makeApiCall("Arsenal FC");
-makeApiCall("Chelsea FC");
-makeApiCall("Liverpool FC");
-makeApiCall("Manchester United");
-makeApiCall("Manchester City FC");
+ console.log(JSON.stringify(event))
   
   //Extract data from event
   for(let record of event.Records){
@@ -111,14 +44,14 @@ makeApiCall("Manchester City FC");
       
       
     //Have sentiment, team and timstamp  
-    const sentiment = JSON.stringify(response.data.label);
+    const sentiment = JSON.stringify(response.data);
     console.log(`Sentiment: ${JSON.parse(sentiment)}\n`);
 
       
       
       //Save result in Sentiment table 
       const command = new PutCommand({
-          TableName: "Sentiment",
+          TableName: "Sentiments",
           Item: {
             "MatchTS": parseInt(timestamp),
             "TeamName": team,
